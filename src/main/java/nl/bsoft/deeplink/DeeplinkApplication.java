c39bd8d4045debf84c9d92a395b8d4229f379d05
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2024-05-27 19:42:04 +0200

    First commit

---
+++ src/main/java/nl/bsoft/deeplink/DeeplinkApplication.java
@@ -0,0 +1,13 @@
+package nl.bsoft.deeplink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeeplinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeeplinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/bsoft/deeplink/DeeplinkApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.bsoft.deeplink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeeplinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
